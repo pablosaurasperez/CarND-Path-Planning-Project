6496e578724c055475215f0804e18df483fd874b
Pablo Sauras <psauras@g.clemson.edu>
2018-02-11 22:14:17 -0700

    Slow down if close to collision, speed up if not. Avoid jerk by starting at vel=0

--- src/main.cpp
+++ src/main.cpp
@@ -205,7 +205,8 @@ int main() {
   int lane = 1;
   
   //target reference velocity
-  double ref_vel = 49.5;  //mph
+  //double ref_vel = 49.5;  //mph
+  double ref_vel = 0;  //mph.. to avoid sudden acceleration at the begining
 
   h.onMessage([&ref_vel,&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
@@ -246,6 +247,52 @@ int main() {
           	
           	//This helps for doing smooth transitions
           	int prev_size = previous_path_x.size();
+          	
+          	/*********BEGIN AVOID COLLISION ***********/
+          	if(prev_size > 0)
+          	{
+          	  car_s = end_path_s;
+          	}
+          	
+          	bool too_close = false;
+          	
+          	//find ref_y to use
+          	for(int i = 0; i < sensor_fusion.size(); i++)
+          	{
+          	  //car is in my lane
+          	  float d = sensor_fusion[i][6];
+          	  if(d< (2+4*lane+2) && d > (2+4*lane-2))
+          	  {
+          	    double vx = sensor_fusion[i][3];
+          	    double vy = sensor_fusion[i][4];
+          	    double check_speed = sqrt(vx*vx + vy*vy);
+          	    double check_car_s = sensor_fusion[i][5];
+          	    
+          	    check_car_s += ((double)prev_size*.02*check_speed); //if using previous points can project s value out
+          	    
+          	    //check s values greater than mine and s gap
+          	    if((check_car_s > car_s) && ((check_car_s-car_s) < 30))
+          	    {
+          	    
+          	    
+          	      //Do some logic here, lower reference velocity so we don't crsh into the car in front of us, could also flag to try to change lanes
+          	      //ref_vel = 29.5; //mph
+          	      too_close = true; //flag so that if I'm too close instead of reduce to a constant speed I adapt it
+          	      
+          	    }
+          	  }
+          	}
+          	
+          	//I adapt the speed if I am close to collision or not.
+          	if(too_close)
+          	{
+          	  ref_vel -= .224;
+          	}
+          	else if(ref_vel < 49.5)
+          	{
+          	  ref_vel += .224;
+          	}
+          	
 
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
